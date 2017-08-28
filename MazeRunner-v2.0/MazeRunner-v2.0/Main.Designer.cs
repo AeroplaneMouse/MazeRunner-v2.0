3f1f1f6b9974c570fb1eb30d3eef21abd0937756
Daniel Fly <danielfly42@gmail.com>
2017-08-28 13:56:02 +0200

    Created Visual Studio solution

---
+++ MazeRunner-v2.0/MazeRunner-v2.0/Main.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace MazeRunner_v2._0
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_upload = new System.Windows.Forms.Button();
+            this.btn_solve = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btn_upload
+            // 
+            this.btn_upload.Location = new System.Drawing.Point(12, 41);
+            this.btn_upload.Name = "btn_upload";
+            this.btn_upload.Size = new System.Drawing.Size(214, 68);
+            this.btn_upload.TabIndex = 0;
+            this.btn_upload.Text = "Upload";
+            this.btn_upload.UseVisualStyleBackColor = true;
+            this.btn_upload.Click += new System.EventHandler(this.btn_upload_Click);
+            // 
+            // btn_solve
+            // 
+            this.btn_solve.Location = new System.Drawing.Point(232, 41);
+            this.btn_solve.Name = "btn_solve";
+            this.btn_solve.Size = new System.Drawing.Size(214, 68);
+            this.btn_solve.TabIndex = 1;
+            this.btn_solve.Text = "Solve";
+            this.btn_solve.UseVisualStyleBackColor = true;
+            this.btn_solve.Click += new System.EventHandler(this.btn_solve_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(954, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 944);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btn_solve);
+            this.Controls.Add(this.btn_upload);
+            this.Name = "Main";
+            this.Text = "Maze Runner v2.0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_upload;
+        private System.Windows.Forms.Button btn_solve;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ MazeRunner-v2.0/MazeRunner-v2.0/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MazeRunner_v2._0
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btn_upload_Click(object sender, EventArgs e)
+        {
+            // Find image
+        }
+
+        private void btn_solve_Click(object sender, EventArgs e)
+        {
+            // Convert image to array
+            // Find shortest route
+            // Draw path to image
+            // Create new image with shortest path
+            // Message "Maze solved" 
+
+        }
+    }
+}
