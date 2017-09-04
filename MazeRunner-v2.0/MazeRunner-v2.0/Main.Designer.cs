fd5ec541275529465dbcf61ecce9f40e8a4f889c
Daniel Fly <danielfly42@gmail.com>
2017-09-04 10:15:43 +0200

    Added function to load an image. Added a pictureBox to see the loaded maze and solved maze.

--- MazeRunner-v2.0/MazeRunner-v2.0/Main.Designer.cs
+++ MazeRunner-v2.0/MazeRunner-v2.0/Main.Designer.cs
@@ -28,20 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_upload = new System.Windows.Forms.Button();
+            this.btn_load = new System.Windows.Forms.Button();
             this.btn_solve = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txt_x = new System.Windows.Forms.TextBox();
+            this.txt_y = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // btn_upload
+            // btn_load
             // 
-            this.btn_upload.Location = new System.Drawing.Point(12, 41);
-            this.btn_upload.Name = "btn_upload";
-            this.btn_upload.Size = new System.Drawing.Size(214, 68);
-            this.btn_upload.TabIndex = 0;
-            this.btn_upload.Text = "Upload";
-            this.btn_upload.UseVisualStyleBackColor = true;
-            this.btn_upload.Click += new System.EventHandler(this.btn_upload_Click);
+            this.btn_load.Location = new System.Drawing.Point(12, 41);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(214, 68);
+            this.btn_load.TabIndex = 0;
+            this.btn_load.Text = "Load";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
             // 
             // btn_solve
             // 
@@ -60,25 +64,55 @@
             this.progressBar1.Size = new System.Drawing.Size(954, 23);
             this.progressBar1.TabIndex = 2;
             // 
+            // txt_x
+            // 
+            this.txt_x.Location = new System.Drawing.Point(12, 139);
+            this.txt_x.Name = "txt_x";
+            this.txt_x.Size = new System.Drawing.Size(81, 26);
+            this.txt_x.TabIndex = 3;
+            // 
+            // txt_y
+            // 
+            this.txt_y.Location = new System.Drawing.Point(12, 171);
+            this.txt_y.Name = "txt_y";
+            this.txt_y.Size = new System.Drawing.Size(81, 26);
+            this.txt_y.TabIndex = 4;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 203);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(954, 729);
+            this.pictureBox.TabIndex = 5;
+            this.pictureBox.TabStop = false;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(978, 944);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.txt_y);
+            this.Controls.Add(this.txt_x);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.btn_solve);
-            this.Controls.Add(this.btn_upload);
+            this.Controls.Add(this.btn_load);
             this.Name = "Main";
             this.Text = "Maze Runner v2.0";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btn_upload;
+        private System.Windows.Forms.Button btn_load;
         private System.Windows.Forms.Button btn_solve;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox txt_x;
+        private System.Windows.Forms.TextBox txt_y;
+        private System.Windows.Forms.PictureBox pictureBox;
     }
 }
 
--- MazeRunner-v2.0/MazeRunner-v2.0/Main.cs
+++ MazeRunner-v2.0/MazeRunner-v2.0/Main.cs
@@ -1,33 +1,126 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace MazeRunner_v2._0
 {
     public partial class Main : Form
     {
+        private int[,] array = new int[1000,1000];
+        private int width = 0;
+        private int height = 0;
+        private Bitmap maze;
+        private int[] startPos = { 0, 0 };
+
         public Main()
         {
             InitializeComponent();
-
         }
         
 
-        private void btn_upload_Click(object sender, EventArgs e)
+        private void btn_load_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
+                bool succed = true;
                 try
                 {
-
+                    maze = (Bitmap)Image.FromFile(openFileDialog.FileName);
+                    width = maze.Width;
+                    height = maze.Height;
+                }
+                catch (Exception)
+                {
+                    succed = false;
+                    MessageBox.Show("Error");
+                }
+                if (succed)
+                {
+                    startPos = findOpeningOnSide(1, new int[] { 0, 0 });
+                    pictureBox.Image = maze;
+                    if (startPos == new int[] { 0, 0 })
+                    {
+                        MessageBox.Show("Could not find start and end!", "Error");
+                        return;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Image loaded correctly.");
+                    }
                 }
             }
             
             // Find image
         }
 
+        private int[] findOpeningOnSide(int orientation, int[] startSidePos)
+        {
+            // Vertical orientation = 1
+            // Horiansontal orientation = 0
+            int whiteCounter = 0;
+            bool whiteHasStarted = false;
+            int[] whiteStartPos = { startSidePos[0], 0 };
+
+            if (orientation == 1)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (getColor(startSidePos[0], y) == 1)
+                    {
+                        if (!whiteHasStarted) whiteStartPos[1] = y;
+                        array[startSidePos[0], y] = 1;
+                        whiteCounter++;
+                        whiteHasStarted = true;
+                    }
+                    else if (whiteHasStarted) break;
+                }
+                // Returning starting position for maze.
+                if (whiteHasStarted && (whiteCounter == 1 || whiteCounter == 2)) return whiteStartPos;
+                else if (whiteHasStarted && whiteCounter > 2) return new int[] { startSidePos[0], (whiteCounter / 2 + whiteStartPos[1]) };
+                else return new int[] { 0, 0 };
+            }
+            else
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (getColor(x, startSidePos[1]) == 1)
+                    {
+                        if (!whiteHasStarted) whiteStartPos[0] = x;
+                        array[x, startSidePos[1]] = 1;
+                        whiteCounter++;
+                        whiteHasStarted = true;
+                    }
+                    else if (whiteHasStarted) break;
+                }
+                // Returning starting position for maze.
+                if (whiteHasStarted && (whiteCounter == 1 || whiteCounter == 2)) return whiteStartPos;
+                else if (whiteHasStarted && whiteCounter > 2) return new int[] { (whiteCounter / 2 + whiteStartPos[0]), startSidePos[1] };
+                else return new int[] { 0, 0 };
+            }
+        }
+
+        private int getColor(int x, int y)
+        {
+            Color color = maze.GetPixel(x,y);
+            if (color.R < 10 && color.G < 10 && color.B < 10) return 0;
+            else return 1;
+        }
+
         private void btn_solve_Click(object sender, EventArgs e)
         {
+            int x = Convert.ToInt32(txt_x.Text);
+            int y = Convert.ToInt32(txt_y.Text);
+            Bitmap image = (Bitmap)Image.FromFile(@"C:\Users\danie\GitHub\MazeRunner-v2.0\maze.png", true);
+            Color pixelColor = image.GetPixel(x, y);
+
+            int r = pixelColor.R;
+            int g = pixelColor.G;
+            int b = pixelColor.B;
+            MessageBox.Show(r + ":" + g + ":" + b);
+
+
+
             // Convert image to array
             // Find shortest route
             // Draw path to image
